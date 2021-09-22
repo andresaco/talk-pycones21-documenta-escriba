677eed398ec97436691d7670102287bcbf54a531
J. Andrés Pizarro <joseandres.pizarro@datiobbva.com>
2021-09-22 19:11:52 +0200

    PEP8 compliant code

---
+++ calc/dummy.py
@@ -0,0 +1,23 @@
+
+def sum(a: int, b: int) -> int:
+    """
+    Let's sum two integers!
+
+    :param a: An integer
+    :param b: Another integer
+
+    :return: the sum of both parameters
+    """
+    return a + b
+
+
+def div(a: int, b: int) -> int:
+    """
+    Division, mind the zero!
+
+    :param a: An integer
+    :param b: Another integer
+
+    :return: the result of dividing a and b
+    """
+    return a / b
